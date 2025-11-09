1c1d437c10d9f03434c7a5ab3d3755775cedb62b
depash nepal <depash2003@gmail.com>
2025-11-09 16:28:08 -0500

    loading and scaling/normalizing datasets using pytorch

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import pandas as pd
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
-from tensorflow.keras import layers, models
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.4688, 0.4635, 0.3434],
+        std=[0.2033, 0.2051, 0.1967]
+    )
+])
+
+train_data = datasets.ImageFolder(root='backend/data/train', transform=transform)
+val_data = datasets.ImageFolder(root='backend/data/validation', transform=transform)
+test_data = datasets.ImageFolder(root='backend/data/test', transform=transform)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+val_loader   = DataLoader(val_data, batch_size=32, shuffle=False)
+test_loader  = DataLoader(test_data, batch_size=32, shuffle=False)
