c0153f9b80fc600827c1ef2f684a545a01c43901
depash nepal <depash2003@gmail.com>
2025-12-01 22:34:32 -0500

    done generating model 98.44% acc 0.0410 loss

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import torchvision as models
+import torchvision.models as models
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Step 1 make a tranfom that will be used to turn the images into vectores and it will resize,convert to tensor, then normalize
@@ -45,97 +45,33 @@ if torch.cuda.is_available():
 # Step 4 Creating the model itself with what each layer does and how
 
 class VegetableCNN(nn.Module):
-    def __init__(self, num_classes=44, conv_dropout_p=0.0, fc_dropout_p=0.5):
+    def __init__(self, num_classes=44):
         super(VegetableCNN, self).__init__()
         self.backbone = models.resnet18(weights="IMAGENET1K_V1")
-        self.backbone = nn.Linear(self.backbone.fc.in_features, num_classes)
-
-    #     self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
-    #     self.bn1 = nn.BatchNorm2d(32)
-
-    #     self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-    #     self.bn2 = nn.BatchNorm2d(64)
-
-    #     self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-    #     self.bn3 = nn.BatchNorm2d(128)
-
-    #     self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-    #     self.bn4 = nn.BatchNorm2d(256)
-
-    #     self.conv_dropout = nn.Dropout2d(p=conv_dropout_p)
-
-    #     self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-    #     with torch.no_grad():
-    #         dummy = torch.zeros(1, 3, 224, 224)
-    #         dummy_out = self._forward_conv(dummy)
-    #         self.flatten_dim = dummy_out.numel()
-
-    #     self.fc1 = nn.Linear(self.flatten_dim, 512)
-    #     self.fc_bn1 = nn.BatchNorm1d(512)
-
-    #     self.fc2 = nn.Linear(512, 256)
-    #     self.fc_bn2 = nn.BatchNorm1d(256)
-
-    #     self.fc_out = nn.Linear(256, num_classes)
-
-    #     self.fc_dropout = nn.Dropout(fc_dropout_p)
         
-    # def _forward_conv(self, x):
-    #     x = self.conv1(x)
-    #     x = self.bn1(x)
-    #     x = F.relu(x)
-    #     x = self.pool(x)
+        in_features = self.backbone.fc.in_features
+        self.backbone.fc = nn.Linear(in_features, num_classes)
 
-    #     x = self.conv2(x)
-    #     x = self.bn2(x)
-    #     x = F.relu(x)
-    #     x = self.pool(x)
-
-    #     x = self.conv3(x)
-    #     x = self.bn3(x)
-    #     x = F.relu(x)
-    #     x = self.conv_dropout(x)
-    #     x = self.pool(x)
-
-    #     x = self.conv4(x)
-    #     x = self.bn4(x)
-    #     x = F.relu(x)
-    #     x = self.conv_dropout(x)
-    #     x = self.pool(x)
-
-    #     return x
-    
-    # def forward(self, x):
-    #     x = self._forward_conv(x)
-
-    #     x = x.view(x.size(0), -1)
-
-    #     x = self.fc1(x)
-    #     x = self.fc_bn1(x)
-    #     x = F.relu(x)
-    #     x = self.fc_dropout(x)
-
-    #     x = self.fc2(x)
-    #     x = self.fc_bn2(x)
-    #     x = F.relu(x)
-    #     x = self.fc_dropout(x)
-
-    #     x = self.fc_out(x)
-    #     return self.backbone(x)
+    def forward(self, x):
+        return self.backbone(x)
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Step 5 pulls model down and puts it in the cpu/gpu and determine what algorithm to use
 
 model = VegetableCNN().to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+# [Note]------------------------------------------------
+# When to change LR in optimizer:
+# Loss jumps violently → LR too high
+# Validation improves very slowly → LR too low
+# Accuracy rises then collapses → too high
+# --------------------------------------------------------
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Step 6 detirmine how many loops you run through and pulling data and putting it into the model to train
 
-num_epochs = 20
+num_epochs = 15
 
 for epoch in range(num_epochs):
     running_loss = 0.0
@@ -163,8 +99,6 @@ for epoch in range(num_epochs):
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-
-    scheduler.step()
 
     epoch_loss = running_loss / len(train_loader)
     epoch_acc = 100 * correct / total
@@ -208,4 +142,4 @@ print(
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Step 8 Saveing trained data
 
-# torch.save(model.state_dict(), "fruit_vegetable_model.pth")
+torch.save(model.state_dict(), "fruit_vegetable_model.pth")
